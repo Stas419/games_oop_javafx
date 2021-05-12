920b27a63eefd6b183544640e75d0e3bdf2b83d2
stanislav <1991>
2021-05-12 12:50:08 +0300

    2. Сокобан. Тестовое задание. [#405541]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,34 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if(board[i][i] == 1 && gorizont(board, i) || vertical(board, i)){
+                rsl = true ;
+            }
+        }
         return rsl;
+    }
+
+    public static boolean gorizont(int[][] board, int x) {
+        boolean a = true;
+        for (int i = 0; i < board.length; i++) {
+            if(board[i][x] != 1){
+                a = false;
+                break;
+            }
+        }
+        return a;
+    }
+
+    public static boolean vertical(int[][] board, int x) {
+        boolean a = true;
+        for (int i = 0; i < board.length; i++) {
+            if(board[x][i] != 1){
+                a = false;
+                break;
+            }
+        }
+        return a;
     }
 }
